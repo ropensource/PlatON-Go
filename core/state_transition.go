dea9086b0b29d4949f9473e1b0fe099650cffb6b
Your Name <you@example.com>
2019-06-27 16:24:05 +0800

    modify tx_pool.go for shield contract created.

--- core/state_transition.go
+++ core/state_transition.go
@@ -18,7 +18,6 @@ package core
 
 import (
 	"errors"
-	"fmt"
 	"math"
 	"math/big"
 
@@ -204,11 +203,6 @@ func (st *StateTransition) TransitionDb() (ret []byte, usedGas uint64, failed bo
 		// error.
 		vmerr error
 	)
-
-	// todo: shield contract to created in temporary
-	if contractCreation {
-		return nil, 0, false, fmt.Errorf("contract creation is not allowed")
-	}
 
 	if contractCreation {
 		ret, _, st.gas, vmerr = evm.Create(sender, st.data, st.gas, st.value)
